9d4cf0e27f863972f572a980bd9dc17e423630b0
Drew Warkentin <drewdulz@gmail.com>
2015-05-30 21:57:08 -0400

    completed logical implementation
    styling remains

--- src/js/app.js
+++ src/js/app.js
@@ -8,14 +8,30 @@ var ajax = require('ajax');
 var loadingWindow = new UI.Window({
 	fullscreen: true,
 });
-var textfield = new UI.Text({
-	position: new Vector2(0, 45),
-	size: new Vector2(144, 30),
-	font: 'gothic-24-bold',
-	text: 'Loading Beers on Sale...',
-	textAlign: 'center'
+
+var background = new UI.Rect({
+	position: new Vector2(0, 0),
+	size: new Vector2(144, 166),
+	backgroundColor: 'white'
 });
+var textfield = new UI.Text({
+	position: new Vector2(0, 0),
+	size: new Vector2(144, 20),
+	font: 'gothic-24-bold',
+	text: 'Loading...',
+	textAlign: 'center',
+  color: 'black'
+});
+var loadingImage = new UI.Image({
+	position: new Vector2(8, 35),
+	size: new Vector2(128, 128),
+	backgroundColor: 'clear',
+	image: 'images/loading.png'
+});
+
+loadingWindow.add(background);
 loadingWindow.add(textfield);
+loadingWindow.add(loadingImage);
 loadingWindow.show();
 
 // Perform a GET requsest to the Ontario beer API.
@@ -70,12 +86,13 @@ var showMoreInfo = function(beerList, index) {
 			// construct the new window for more beer info
 			var moreInfo = new UI.Window({
 				fullscreen: true,
+				scrollable: true
 			});
 			// Construct the more info panel
 			// Title
 			var title = new UI.Text({
-				position: new Vector2(20, 0),
-				size: new Vector2(124, 20),
+				position: new Vector2(5, 0),
+				size: new Vector2(144, 20),
 				font: 'gothic-18-bold',
 				text: data[0].name,
 				textOverflow: 'ellipsis',
@@ -84,8 +101,8 @@ var showMoreInfo = function(beerList, index) {
 
 			// Percent Alcohol
 			var percentAlc = new UI.Text({
-				position: new Vector2(20, 20),
-				size: new Vector2(124, 10),
+				position: new Vector2(5, 20),
+				size: new Vector2(144, 20),
 				font: 'gothic-14',
 				text: "% Alcohol: " + data[0].abv,
 				textAlign: 'left'
@@ -93,22 +110,38 @@ var showMoreInfo = function(beerList, index) {
 
 			// Pricing Title
 			var priceTitle = new UI.Text({
-				position: new Vector2(20, 30),
-				size: new Vector2(124, 20),
+				position: new Vector2(5, 40),
+				size: new Vector2(144, 20),
 				font: 'gothic-18',
-				text: "Pricing",
+				text: "Pricing:",
 				textAlign: 'left'
 			});
 
 			// Generate the pricing table:
 			var priceTable = [];
 			data.forEach(function(product, index) {
+				// Parse the string to make the text simpler for displaying on a small screen.
+				var productSize = product.size;
+				if(productSize.search("Can") > 0) {
+					productSize = "ITS A CAN";
+				}
+				// productSize = productSize.toString();
+				// console.log(product.size);
+				// console.log("60  ×  Can 355 ml");
+				// productSize = "60  ×  Can 355 ml";
+				// productSize = productSize.replace(/Bottle 341 ml/g, "Bottles");
+				// productSize = productSize.replace(/Can 355 ml/g, "Cans");
+				// productSize = productSize.replace(/Can 473 ml/g, "Bottles");
+
+				// var newString = '24  ×  Bottle 341 ml';
+    		// newString = newString.replace(/Bottle 341 ml/g, "Bottles");
+
 				// generate an array of price information
 				priceTable[index] = new UI.Text({
-					position: new Vector2(20, 50 + index * 10),
-					size: new Vector2(124, 10),
+					position: new Vector2(5, 60 + index * 15),
+					size: new Vector2(144, 15),
 					font: 'gothic-14',
-					text: "$" + product.price + " for " + product.size,
+					text: "$" + product.price + " for " + productSize,
 					textOverflow: 'fill',
 					textAlign: 'left'
 				})
